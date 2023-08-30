0af0c421f5345169990c306f57698ae83e55b239
purple1703 <purple1703@naver.com>
2023-08-30 15:49:58 +0900

    초기설정2

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 하하
+          Learn React 하하하하하ㅏ
         </a>
       </header>
     </div>
