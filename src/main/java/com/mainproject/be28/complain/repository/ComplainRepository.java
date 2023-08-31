76530852537e2e70894f9c4b8f79bb7927f9d2e1
ararp1006 <ararp1006@gmail.com>
2023-08-31 18:32:32 +0900

    crud

--- src/main/java/com/mainproject/be28/complain/repository/ComplainRepository.java
+++ src/main/java/com/mainproject/be28/complain/repository/ComplainRepository.java
@@ -2,10 +2,8 @@ package com.mainproject.be28.complain.repository;
 
 import com.mainproject.be28.complain.entity.Complain;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ComplainRepository extends JpaRepository<Complain, Long> {
