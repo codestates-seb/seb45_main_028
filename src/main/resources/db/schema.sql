814137bc4f886f499ce0dbb624da0813d3c55e81
hjwnu <130176882+hjwnu@users.noreply.github.com>
2023-09-01 15:22:16 +0900

    Update schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -85,6 +85,8 @@ CREATE TABLE COMPLAIN (
                           MEMBER_ID BIGINT NOT NULL,
                           ITEM_ID BIGINT NOT NULL,
                           CONTENT VARCHAR(1000),
+                          CREATED_AT TIMESTAMP,
+                          LAST_MODIFIED_AT TIMESTAMP,
                           FOREIGN KEY (MEMBER_ID) REFERENCES MEMBER(MEMBER_ID),
                           FOREIGN KEY (ITEM_ID) REFERENCES ITEM(ITEM_ID)
 );
