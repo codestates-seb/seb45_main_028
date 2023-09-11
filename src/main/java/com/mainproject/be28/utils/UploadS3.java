d4d1d17aedba7c7eebc66baa7df559b978e67ef3
hjwnu <tlstjr01@gmail.com>
2023-09-11 21:26:34 +0900

    feat S3 Config, add ExceptionCode

---
+++ src/main/java/com/mainproject/be28/utils/UploadS3.java
@@ -0,0 +1,65 @@
+package com.mainproject.be28.utils;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.mainproject.be28.exception.BusinessLogicException;
+import com.mainproject.be28.exception.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UploadS3 {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public String upload(MultipartFile multipartFile, String fileName) throws IOException{
+    // 이미지 변환
+        File uploadFile = convert(multipartFile).orElseThrow(() -> new BusinessLogicException(ExceptionCode.IMAGE_NOT_CONVERTED));
+    // s3로 업로드
+        String uploadImageUrl = putS3(uploadFile, fileName);
+    //  로컬 삭제
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    // 로컬에 저장된 이미지 지우기
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("File delete success");
+            return;
+        }
+        log.info("File delete fail");
+    }
+
+    private Optional<File> convert(MultipartFile multipartFile) throws IOException{
+        File convertFile = new File(System.getProperty("user.dir") + "/" + multipartFile.getOriginalFilename());
+        // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
+                fos.write(multipartFile.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+
+    }
+}
