a68b4ba48a7883434c72e63e2aca9590916c0d5b
hjwnu <tlstjr01@gmail.com>
2023-09-12 10:48:16 +0900

    feat  make Swagger

---
+++ src/main/java/com/mainproject/be28/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.mainproject.be28.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mainproject"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .version("1.0.0")
+                .description("개발자들 swagger api")
+                .build();
+    }
+}
