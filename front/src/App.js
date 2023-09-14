d5b065f7d458000aa0d598160a863d427a248b3a
Leesaeda <leesaeda@gmail.com>
2023-09-14 14:53:02 +0900

    불필요한 파일 삭제, eslint prettier 설치 및 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import '../src/css/input.css'
+import '../src/css/input.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
-import Main from './Pages/Main.jsx'
-import About from './Pages/About.jsx'
+import Main from './Pages/Main.jsx';
+import About from './Pages/About.jsx';
 import Annoucement from './Pages/Announcement.jsx';
 
 function App() {
@@ -14,15 +14,14 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route path='/Main' element={<Main />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Announcement' element={<Annoucement />} />
+          <Route path="/Main" element={<Main />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Announcement" element={<Annoucement />} />
         </Routes>
         <Footer />
       </div>
     </BrowserRouter>
   );
 }
-
 
 export default App;
--- front/src/Components/Footer.jsx
+++ front/src/Components/Footer.jsx
@@ -11,4 +11,4 @@ function Footer() {
   );
 }
 
-export default Footer; 
+export default Footer;
