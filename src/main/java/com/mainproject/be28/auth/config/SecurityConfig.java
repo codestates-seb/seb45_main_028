3d8faa62f3f6267bc7f101248063b47d4b237ff5
hjwnu <tlstjr01@gmail.com>
2023-09-18 21:24:29 +0900

    fix cors

--- src/main/java/com/mainproject/be28/auth/config/SecurityConfig.java
+++ src/main/java/com/mainproject/be28/auth/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import com.mainproject.be28.auth.userdetails.UserDetailService;
 import com.mainproject.be28.member.repository.MemberRepository;
 import com.mainproject.be28.member.service.MemberService;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -110,7 +109,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000","http://ec2-52-79-52-23.ap-northeast-2.compute.amazonaws.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000","http://ec2-52-79-52-23.ap-northeast-2.compute.amazonaws.com","http://28be.s3-website.ap-northeast-2.amazonaws.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "PATCH", "DELETE", "POST", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Refresh"));
